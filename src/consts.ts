6ed397a643c9b9a8182459f18b0cdd063fc1930a
joane325 <nevetha23a@gmail.com>
2025-06-23 20:48:51 -0400

    edit post display

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Reckoner of MGCI';
+export const SITE_DESCRIPTION = 'Marc Garneau CI\'s only student-run newspaper.';
